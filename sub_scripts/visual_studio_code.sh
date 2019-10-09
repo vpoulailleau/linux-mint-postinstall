745137f8d9b6ef03cfb97f5c310c3348ed4e9fee
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-10-09 12:26:48 +0200

    add VS code extensions

--- sub_scripts/visual_studio_code.sh
+++ sub_scripts/visual_studio_code.sh
@@ -8,3 +8,19 @@ sudo apt update -y
 sudo apt install -y code
 # sudo chown -R $(whoami) /usr/share/code
 code --install-extension ms-python.python
+code --install-extension visualstudioexptteam.vscodeintellicode
+code --install-extension ms-vscode.cpptools
+code --install-extension coenraads.bracket-pair-colorizer
+code --install-extension compulim.compulim-vscode-closetag
+code --install-extension batisteo.vscode-django
+code --install-extension bibhasdn.django-html
+code --install-extension donjayamanne.githistory
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension dongli.python-preview
+code --install-extension lextudio.restructuredtext
+code --install-extension gruntfuggly.todo-tree
+code --install-extension vscodevim.vim
+code --install-extension vscode-icons-team.vscode-icons
+code --install-extension tomoki1207.pdf
+code --install-extension dotjoshjohnson.xml
