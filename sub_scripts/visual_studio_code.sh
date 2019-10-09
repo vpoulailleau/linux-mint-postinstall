7d9e13797217284b7d93ae00f29cc3038d2fa7f9
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-10-09 16:42:18 +0200

    configure visual studio code

--- sub_scripts/visual_studio_code.sh
+++ sub_scripts/visual_studio_code.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# TODO install python
-
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
 sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
@@ -27,4 +25,82 @@ code --install-extension --force vscode-icons-team.vscode-icons
 code --install-extension --force tomoki1207.pdf
 code --install-extension --force dotjoshjohnson.xml
 
-# TODO config JSON
+
+#############################################
+# Configure VS code
+#############################################
+
+# first install Python, to parse easily JSON
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+eval "$DIR/python.sh"
+
+# create python script
+cat <<EOT >> config_vscode.py
+import json
+import re
+from pathlib import Path
+
+home = Path.home()
+config_json = home / ".config/Code/User/settings.json"
+
+
+def comment_remover(text):
+    def replacer(match):
+        s = match.group(0)
+        if s.startswith("/"):
+            return " "  # note: a space and not an empty string
+        else:
+            return s
+
+    pattern = re.compile(
+        r'//.*?$|/\*.*?\*/|\'(?:\\.|[^\\\'])*\'|"(?:\\.|[^\\"])*"',
+        re.DOTALL | re.MULTILINE,
+    )
+    return re.sub(pattern, replacer, text)
+
+
+config = None
+with open(config_json, encoding="utf-8") as file:
+    content = file.read()
+    content = comment_remover(content)
+    config = json.loads(content)
+    config["breadcrumbs.enabled"] = True
+    config["diffEditor.ignoreTrimWhitespace"] = False
+    config["editor.cursorStyle"] = "line"
+    config["editor.formatOnSave"] = True
+    config["editor.formatOnPaste"] = True
+    config["files.watcherExclude"] = {
+        "**/.git/objects/**": True,
+        "**/.git/subtree-cache/**": True,
+        "**/node_modules/*/**": True,
+    }
+    config["python.editor.formatOnPaste"] = False
+    config["python.pythonPath"] = "/usr/bin/python3.8"
+    config["python.linting.enabled"] = True
+    config["python.linting.flake8Enabled"] = True
+    config["python.linting.flake8Path"] = f"~/.local/bin/whatalinter"
+    config["python.formatting.provider"] = "black"
+    config["python.formatting.blackPath"] = "~/.local/bin/whataformatter"
+    config["python.formatting.blackArgs"] = []
+    # TODO supprimer ces deux lignes
+    config[
+        "python.formatting.blackPath"
+    ] = "~/.local/pipx/venvs/python-dev-tools/bin/black"
+    config["python.formatting.blackArgs"] = [
+        "--line-length",
+        "79",
+        "--target-version=py36",
+    ]
+    config["terminal.integrated.rendererType"] = "dom"
+    config["vim.incsearch"] = True
+    config["vim.useSystemClipboard"] = True
+    config["vim.hlsearch"] = True
+    config["vim.handleKeys"] = {"<C-a>": False, "<C-f>": False}
+    config["workbench.colorTheme"] = "Visual Studio Dark"
+
+if config:
+    with open(config_json, encoding="utf-8", mode="w") as file:
+        json.dump(config, file, indent=4)
+EOT
+
+python3.8 config_vscode.py
