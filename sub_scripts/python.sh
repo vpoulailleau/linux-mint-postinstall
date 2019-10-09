637dbbafc2a6c56ba7fb43c44666b36009cdc94d
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-10-09 14:57:07 +0200

    install python-dev-tools

--- sub_scripts/python.sh
+++ sub_scripts/python.sh
@@ -5,3 +5,17 @@ sudo apt install -y python3.7 python3.7-venv python3-pip python3-venv
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt update -y
 sudo apt install -y python3.8 python3.8-venv
+
+# install pipx
+python3.8 -m pip install --user --upgrade pipx
+python3.8 -m pipx ensurepath
+
+# install userpath
+pipx install userpath
+pipx upgrade userpath
+
+# install python-dev-tools
+pipx install python-dev-tools
+pipx upgrade python-dev-tools
+TOOLS_PATH=$(ls -l ~/.local/bin/whataformatter | sed -e "s/.*-> //" | sed -e "s#/bin.*#/bin#")
+userpath prepend $TOOLS_PATH
