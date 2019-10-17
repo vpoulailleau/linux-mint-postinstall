238506e517f3846e936b9edbe2e665e3b81ebaa2
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-10-17 10:06:34 +0200

    use opale3.7

--- sub_scripts/opale.sh
+++ sub_scripts/opale.sh
@@ -2,4 +2,4 @@
 
 echo "deb https://download.scenari.org/deb bionic main" | sudo tee /etc/apt/sources.list.d/scenari.list
 sudo apt update
-sudo apt install -y opale3.6.fr-fr texlive texlive-latex-extra dvipng
+sudo apt install -y opale3.7.fr-fr texlive texlive-latex-extra dvipng
