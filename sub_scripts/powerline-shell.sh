47433380d10120b3ebc9ddab4bfd8811b834e3e0
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-10-18 13:00:04 +0200

    add powerline-shell

--- sub_scripts/powerline-shell.sh
+++ sub_scripts/powerline-shell.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-# TODO
+sudo apt install -y fonts-powerline
+pip3 install powerline-shell
+
+INSTALLED=`cat ~/.bashrc | grep "powerline-shell" | wc -l`
+if [ "$INSTALLED" = "0" ]
+then
+    echo ""                                                                  >> ~/.bashrc
+    echo "# https://github.com/vpoulailleau/linux-mint-postinstall"          >> ~/.bashrc
+    echo 'function _update_ps1() {'                                          >> ~/.bashrc
+    echo '    PS1=$(powerline-shell $?)'                                     >> ~/.bashrc
+    echo '}'                                                                 >> ~/.bashrc
+    echo ''                                                                  >> ~/.bashrc
+    echo 'if [[ $TERM != linux && ! $PROMPT_COMMAND =~ _update_ps1 ]]; then' >> ~/.bashrc
+    echo '    PROMPT_COMMAND="_update_ps1; $PROMPT_COMMAND"'                 >> ~/.bashrc
+    echo 'fi'                                                                >> ~/.bashrc
+fi
