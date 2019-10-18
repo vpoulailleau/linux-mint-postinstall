64fb600155cd9644a4c3de830e677b68bc64d9cd
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-10-18 10:37:42 +0200

    add common aliases

--- sub_scripts/aliases.sh
+++ sub_scripts/aliases.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
 # add common aliases
+cat <<EOT > ~/.bash_aliases_linux_mint_postinstall.sh
+alias ls='ls --color=auto'
+
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+
+alias ga='git add'
+alias gc='git commit'
+alias gs='git status'
+alias gl='git log --graph --oneline --decorate --all'
+EOT
+
+INSTALLED=`cat ~/.bashrc | grep bash_aliases_linux_mint_postinstall.sh | wc -l`
+if [ "$INSTALLED" = "0" ]
+then
+    echo ""                                                         >> ~/.bashrc
+    echo "# https://github.com/vpoulailleau/linux-mint-postinstall" >> ~/.bashrc
+    echo "source ~/.bash_aliases_linux_mint_postinstall.sh"         >> ~/.bashrc
+fi
