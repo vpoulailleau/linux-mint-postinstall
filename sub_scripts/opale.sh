271db7bdfa4f4549b8b8ef0aa4e17263e622e189
Vincent Poulailleau <vpoulailleau@gmail.com>
2019-11-15 10:43:19 +0100

    fix key for opale installation

--- sub_scripts/opale.sh
+++ sub_scripts/opale.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-echo "deb https://download.scenari.org/deb bionic main" | sudo tee /etc/apt/sources.list.d/scenari.list
+UBUNTU_VERSION=`cat /etc/os-release | grep UBUNTU_CODENAME`
+UBUNTU_VERSION=${UBUNTU_VERSION#*=}
+
+echo "deb https://download.scenari.org/deb ${UBUNTU_VERSION} main" | sudo tee /etc/apt/sources.list.d/scenari.list
+sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com C1B92A9981DBBE62
 sudo apt update
 sudo apt install -y opale3.7.fr-fr texlive texlive-latex-extra dvipng
